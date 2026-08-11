cb2bf90bf4e88c568b87d3d30f56727c3ddc8488
Zhameer Sheraz Tampugao <zhameersheraztampugao@gmail.com>
2026-08-11 00:49:13 -0400

    v2.42: freeCodeCamp cert (cropped PNG from your screenshot)

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -233,7 +233,7 @@ export const CERTS: Cert[] = [
     title: "Python (Developer Certification)",
     issuer: "freeCodeCamp",
     issued: "Aug 2026",
-    image: "/certs/freecodecamp-python.svg",
+    image: "/certs/freecodecamp-python.png",
     link: "https://freecodecamp.org/certification/zshrc/python-v9",
   },
 ] as const;
